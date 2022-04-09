cbf11ea7360f54a4c2c65887893ac3bb8c759bc5
Allan <allan.lau@temple.edu>
2022-04-09 14:47:05 -0400

    added master/home page and signup page

---
+++ RecipeSite/RecipeSite/SignUp.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RecipeSite
+{
+    public partial class SignUp : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        // create and add user to database
+        protected void btnSignUp_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
