ba40340db0a558c39f956f2f5930a1194549cd77
sssoms <somikim@temple.edu>
2022-04-13 23:25:39 -0400

    added search page

---
+++ RecipeSite/RecipeSite/Search.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/RecipeSite.Master" AutoEventWireup="true" CodeBehind="Search.aspx.cs" Inherits="RecipeSite.Search" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server"></asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+        <div>
+            <asp:ScriptManager ID="ScriptManager1" runat="server">
+            </asp:ScriptManager>
+            <h3>Find a Recipe</h3><br />
+            <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+                <ContentTemplate>
+                    <table>
+                        <tr>
+                            <td>
+                                Search By
+                            </td>
+                            <td>
+                                <asp:DropDownList ID="ddlSearchBy" runat="server" OnSelectedIndexChanged="ddlSearchBy_SelectedIndexChanged" AutoPostBack="True">
+                                    <asp:ListItem Selected="True">Select</asp:ListItem>
+                                    <asp:ListItem>Main Ingredient</asp:ListItem>
+                                    <asp:ListItem>Cooking Method</asp:ListItem>
+                                    <asp:ListItem>Food Category</asp:ListItem>
+                                </asp:DropDownList>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <asp:Label ID="lblSelectedSearchBy" runat="server" Text=""></asp:Label>
+                            </td>
+                            <td>
+                                <asp:DropDownList ID="ddlSelectedSearchBy" runat="server"></asp:DropDownList>
+                            </td>
+                        </tr>
+                    </table>
+                </ContentTemplate>  
+           </asp:UpdatePanel>
+            <br />
+            <asp:Button ID="btnSearch" runat="server" Text="Search" OnClick="btnSearch_Click" />
+        </div>
+</asp:Content>
---
+++ RecipeSite/RecipeSite/Search.aspx.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Utilities;
+
+namespace RecipeSite
+{
+    public partial class Search : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ddlSearchBy_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (ddlSearchBy.SelectedValue != "Select")
+            {
+                lblSelectedSearchBy.Text = ddlSearchBy.SelectedValue;
+            }
+            DataSet ddlDS = GetSelectedSearchByDataSet(ddlSearchBy.SelectedValue);
+
+            ddlSelectedSearchBy.DataSource = ddlDS.Tables[0];
+            ddlSelectedSearchBy.DataValueField = ddlDS.Tables[0].Columns[0].ToString();
+            ddlSelectedSearchBy.DataTextField = ddlDS.Tables[0].Columns[0].ToString();
+            ddlSelectedSearchBy.DataBind();
+        }
+
+        public DataSet GetSelectedSearchByDataSet(string selectedValue)
+        {
+            if (selectedValue == "Select")
+            {
+                return null;
+            }
+
+            DBConnect objDB = new DBConnect();
+            SqlCommand objCommand = new SqlCommand();
+            objCommand.CommandType = CommandType.StoredProcedure;
+            DataSet myDS;
+
+            switch (selectedValue)
+            {
+                case "Main Ingredient":
+                    objCommand.CommandText = "TP_SearchByMainIngredient";
+                    myDS = objDB.GetDataSet(objCommand);
+                    break;
+                case "Cooking Method":
+                    objCommand.CommandText = "TP_SearchByFoodCategories";
+                    myDS = objDB.GetDataSet(objCommand);
+                    break;
+                case "Food Category":
+                    objCommand.CommandText = "TP_SearchByFoodCategories";
+                    myDS = objDB.GetDataSet(objCommand);
+                    break;
+                default:
+                    myDS = null;
+                    break;
+            }
+
+            return myDS;
+        }
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            lblSelectedSearchBy.Text = ddlSearchBy.SelectedValue;
+        }
+    }
+}
